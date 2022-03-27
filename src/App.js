700fd6bf6eeccec77848755488380be85b76f297
RicardoOliveira <ricmenol@outlook.com>
2022-03-27 17:11:26 -0300

    Resolução altura 100vh

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Welcome name="Ricardo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
